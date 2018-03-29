4ed453b90ba5af0d8ac51684fe4fd68a76965df4
smachtmes <smachtmes@gmail.com>
2018-03-29 14:45:09 -0700

    initial

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [
+  {
+    "hostname" => "ec2-18-232-103-63.compute-1.amazonaws.com",
+    "ipaddress" => "18.232.103.63",
+    "port" => 80,
+    "ssl_port" => 80
+  }]
+
+include_recipe 'haproxy::manual'
